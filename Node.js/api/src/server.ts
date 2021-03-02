922de63d90dd971f03227a9022ff86b6ca84602c
Paulo Ricardo Horta Grando <paulorhg@hotmail.com>
2021-03-02 14:41:48 -0300

    test

--- Node.js/api/src/server.ts
+++ Node.js/api/src/server.ts
@@ -1,6 +1,9 @@
-import express from 'express';
+import express, { request, response } from 'express';
 
 const app = express();
 
+app.get("/users", (request, response) =>{
+    return response.json("Hello world")
+})
 
 app.listen(3333, () => console.log("Server is running!"));
